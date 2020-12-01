c4862147a78937106fe3b13ab28ff210c74cbc79
Ethan Massie <ethan@emassie.dev>
2020-12-01 17:47:03 -0600

    updated wanikani-api-ng dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Platform, MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { WanikaniTokenService, UserService } from 'wanikani-api-ng';
-import { Router } from '@angular/router';
+import { MenuController, Platform } from '@ionic/angular';
+import { UserService, WanikaniTokenService } from 'wanikani-api-ng';
+
 
 @Component({
   selector: 'app-root',
@@ -24,19 +24,20 @@ export class AppComponent implements OnInit {
     this.initializeApp();
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.tokenService.getIsAuthenticated().subscribe(
-      (auth)=>{
-        this.menuController.enable(auth,'side')
-        if(!auth){
-          this.userService.clearCache();
-          this.router.navigate(['login'])
-        } else{
+      (auth) => {
+        this.menuController.enable(auth, 'side');
+        if (!auth) {
+          // remove user info
+          this.router.navigate(['login']);
+        } else {
+          // store user info
           this.userService.getUser();
-          this.router.navigate(['home'])
+          this.router.navigate(['home']);
         }
       }
-    )
+    );
   }
 
   initializeApp() {
