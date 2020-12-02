a956c26823b696caf2ad5fd557263156febea726
Joel Wetzell <jwetzell@yahoo.com>
2020-12-01 18:11:52 -0600

    ng update @angular/core @angular/cli

--- src/app/user-info/user-info.component.spec.ts
+++ src/app/user-info/user-info.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { UserInfoComponent } from './user-info.component';
@@ -7,7 +7,7 @@ describe('UserInfoComponent', () => {
   let component: UserInfoComponent;
   let fixture: ComponentFixture<UserInfoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UserInfoComponent ],
       imports: [IonicModule.forRoot()]
